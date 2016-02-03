c1c8ef5946b61e40bde9d080f76aa0a35538af0f
matt venn <matt@mattvenn.net>
2016-02-03 21:55:39 +0100

    menu system

---
+++ twilio/matt_voip/menu.py
@@ -0,0 +1,42 @@
+
+numbers = [ '', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz' ]
+
+class Menu():
+
+    def __init__(self, contacts):
+        self.contacts = contacts
+  
+    def get_options(self, digits):
+        combos = self.recurse_combos(digits)
+        options = []
+        for c in combos:
+            if len(digits) == len(c):
+                if c in self.contacts:
+                    options.append(self.contacts[c])
+        return options 
+
+
+    """
+    1  : a,b,c
+    11 : aa,ab,ac,ba,bb,bc,ca,cb,cc
+    111: aaa,aab,aac,aba,abb,abc,aca,acb,acc.....
+    """
+    def recurse_combos(self, digits, index=0, combos=[]):
+        # base case
+        if index == len(digits):
+            return combos
+
+        assert digits[index].isdigit()
+        digit = int(digits[index])
+        group = numbers[digit]
+
+        new_combos = [g for g in group]
+
+        for c in combos:
+            for l in group:
+                new_combos.append( c + l )
+
+        return self.recurse_combos(digits, index + 1, new_combos)
+
+        
+
---
+++ twilio/matt_voip/test.py
@@ -0,0 +1,83 @@
+import unittest
+from menu import Menu, numbers
+
+def ntd(name):
+    digits = ''
+    for l in name:
+        for g, digit in zip(numbers, range(len(numbers))):
+            if l in g:
+                digits += str(digit)
+    return digits
+   
+test_contacts = {
+    'a'   : 1,
+    'd'   : 2,
+    'ad'  : 3,
+    'da'  : 4,
+    'dad' : 5,
+    'matt': 6,
+    'tum' : 7,
+    'tun' : 8,
+    'uuo' : 9,
+    }
+
+class TestMenu(unittest.TestCase):
+    """
+    @classmethod
+    def setupClass(cls):
+        from control import Control
+        cls._robot = Control(PORT)
+    """
+
+    def test_digit_conv(self):
+        self.assertEqual(ntd('matt'), '6288')
+        self.assertEqual(ntd('aaa'), '222')
+        self.assertEqual(ntd('dad'), '323')
+
+    
+    def test_init(self):
+        m = Menu(test_contacts)
+
+    def test_get_combos(self):
+        m = Menu(test_contacts)
+        combos = m.recurse_combos('222')
+        i = 0
+        for c in combos:
+            if len(c) == 3:
+                i += 1
+        self.assertEqual(i, 3*3*3)
+       
+    def test_bad_digits(self):
+        m = Menu(test_contacts)
+        with self.assertRaises(AssertionError):
+            m.get_options('xx')
+
+    def test_no_digits(self):
+        m = Menu(test_contacts)
+        self.assertEqual(m.get_options(''), [])
+
+    def test_single_options_single_digits(self):
+        m = Menu(test_contacts)
+        assert len(m.get_options('2')) == 1
+        assert m.get_options('2')[0] == 1
+
+        assert len(m.get_options('3')) == 1
+        assert m.get_options('3')[0] == 2
+
+    def test_single_options_multi_digits(self):
+        m = Menu(test_contacts)
+
+        for name in ['matt', 'dad', 'da', 'ad']:
+            self.assertEqual(len(m.get_options(ntd(name))), 1)
+            self.assertIn(test_contacts[name], m.get_options(ntd(name)))
+      
+    def test_multi_options_multi_digits(self):
+        m = Menu(test_contacts)
+
+        for name in ['tum', 'tun', 'uuo']:
+            self.assertEqual(len(m.get_options(ntd(name))), 3)
+            self.assertIn(test_contacts[name], m.get_options(ntd(name)))
+        
+
+if __name__ == '__main__':
+    unittest.main()
